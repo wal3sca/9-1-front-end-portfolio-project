0e6357851c9059fa73b879a40be0ea5d9db8a8af
wal3sca <walescaambroise@gmail.com>
2022-09-14 16:34:05 -0400

    meals by category

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
-const URL = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+const URL = "http://www.themealdb.com/api/json/v1/1/filter.php?c="
 
-// fetches URL
-fetch(URL)
-// convert response to JSON
-.then(res => res.json())
-// log data to tell user that you're able to grab data
-.then(resJson=>console.log(resJson))
-// look for errors
-.catch((err) => console.log(err))
+const form = document.querySelector("form")
+const input = document.querySelector("input")
+
+form.addEventListener("submit", (event)=>{
+    event.preventDefault()
+
+
+    // fetches URL
+    fetch(`${URL}${input.value}`)
+    // convert response to JSON
+    .then(res => res.json())
+    // log data to tell user that you're able to grab data
+    .then(resJson=>{
+        // console.log(resJson)
+        // console.log(resJson.meals)
+        // iterates over the array
+        resJson.meals.forEach(meal => {
+           console.log(meal)
+            // STEPS:
+            // create an element for each meal
+            const img = document.createElement("img")
+            // give that element inner text of that meals name
+            img.innerText("meal")
+            // create an image tag
+            img.src = "images/media/meals/llcbn01574260722.jpg/preview"
+            // give the image tag the src of each meals image 
+            // append the element and the image to your page
+           meal.append("img")
+        });
+
+    })
+    // look for errors
+    .catch((err) => console.log(err))
+
+})
--- proposal.md
+++ proposal.md
@@ -2,9 +2,6 @@
 
 ### User Stories
 
-- As a user, when I visit the website I see a links to multiple pages so that I can navigate from one page to the other
-- As a user I can click on an About link so that it will navigate to a new page where I see information about the developer
-- As a user, information about users is retrieved from an API so that I can see a list of photos to click on
-- As a user, I can type in an input box and search for a photo based on the input, (e.g. cats) so that a new stream of photos will appear based on that theme.
-- As a user, if I try to submit with no entry in the input box, I see a message on the screen telling me to enter a value to search so that I cannot submit without entering a value
-- As a user, I can click on a photo so that I can view a single photo with information about that photo
+- As a user, when I visit the website I see a search form where I can look for meals when I click the search button.
+- As a user I can click on the search button to then retrieve a list of different meals by category (i.e Seafood, Chicken, Beef etc).
+- As a user, I am able to see images of different meals that I've selected show up on the page.
