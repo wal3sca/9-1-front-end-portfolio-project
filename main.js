bb1c2e8bf3b905e48653594120c7be8ba9aaf4d3
wal3sca <walescaambroise@gmail.com>
2022-09-16 09:00:43 -0400

    added about link, added css to the main js page, created a search form button, got the search generator to collect images and data descriptions for each meal

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-const URL = "http://www.themealdb.com/api/json/v1/1/filter.php?c="
+const URL = "https://www.themealdb.com/api/json/v1/1/filter.php?c="
 
+// initialize all tags
 const form = document.querySelector("form")
 const input = document.querySelector("input")
+const div = document.querySelector("div")
+// image
+const section = document.querySelector("section")
 
 form.addEventListener("submit", (event)=>{
     event.preventDefault()
-
 
     // fetches URL
     fetch(`${URL}${input.value}`)
@@ -13,23 +16,31 @@ form.addEventListener("submit", (event)=>{
     .then(res => res.json())
     // log data to tell user that you're able to grab data
     .then(resJson=>{
+        // clear out section
+        section.innerHTML = ""
         // console.log(resJson)
         // console.log(resJson.meals)
         // iterates over the array
         resJson.meals.forEach(meal => {
            console.log(meal)
+
             // STEPS:
             // create an element for each meal
             const img = document.createElement("img")
             // give that element inner text of that meals name
-            img.innerText("meal")
             // create an image tag
-            img.src = "images/media/meals/llcbn01574260722.jpg/preview"
+            img.src = meal.strMealThumb
+            // console.log(meal.strMealThumb)
             // give the image tag the src of each meals image 
             // append the element and the image to your page
-           meal.append("img")
-        });
 
+           section.append(img)
+
+           const p = document.createElement("p")
+        //    updates text inside tag
+           p.innerText = meal.strMeal
+           section.append(p)
+        });
     })
     // look for errors
     .catch((err) => console.log(err))
